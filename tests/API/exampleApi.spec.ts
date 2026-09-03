b7e2998b0600ed0e52520f6f06a33d21ef583c50
Suryakanta-1998 <suryakanta1998@gmail.com>
2026-09-03 18:40:58 +0530

    Added headers to run the API in GitHub Actions

--- tests/API/exampleApi.spec.ts
+++ tests/API/exampleApi.spec.ts
@@ -1,7 +1,13 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('API test example', async ({ request }) => {
-    const response = await request.get('https://fakestoreapi.com/products/1');
+    const response = await request.get('https://fakestoreapi.com/products/1', {
+        //headers added only for GitHub Action, not needed for local testing
+        headers: {
+            Accept: 'application/json',
+            'User-Agent': 'Mozilla/5.0 Playwright-CI',
+        },
+    });
     expect(response.status()).toBe(200);
     const responseBody = await response.json();
     console.log('API response:', responseBody);
